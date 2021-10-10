fe44fa5ce95a0a2fc1fe0fe4e573d78d552f76da
sheikh-spear <sheikh-spear>
2021-10-10 11:39:30 +0200

    Adding dots to the resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 import Skill from "./Skill";
 import axios from "axios";
-import pdf from "../../Assets/Soumyajit-Behera.pdf";
+import pdf from "../../Assets/IsmailHAOUAM.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import nodeJSImg from "../../Assets/nodeJS.png";
 
@@ -50,12 +50,12 @@ function Resume() {
               place="Bois-Colombes, France"
               date="Since October 2020"
               content={[
-                "Planning of the penetration testing campaigns",
-                "Monitoring of penetration tests",
-                "Analysis of audit reports and remediation plan",
-                "Vulnerability monitoring",
-                "Automation of analyzes",
-                "Reproduction of found exploits",
+                "Planning of the penetration testing campaigns.",
+                "Monitoring of penetration tests.",
+                "Analysis of audit reports and remediation plan.",
+                "Vulnerability monitoring.",
+                "Automation of analyzes.",
+                "Reproduction of found exploits.",
               ]}
             />
             <Resumecontent
@@ -65,7 +65,7 @@ function Resume() {
               place="Paris, France"
               date="Since June 2021"
               content={[
-                "Teaching multiple topics to adults going through professional retraining including low level topics such as programming languages to high level topics like database conception or cyber-security",
+                "Teaching multiple topics to adults going through professional retraining including low level topics such as programming languages to high level topics like database conception or cyber-security.",
               ]}
             />
             <Resumecontent
@@ -75,11 +75,11 @@ function Resume() {
               place="Paris, France"
               date="Since June 2021"
               content={[
-                "Single-handedly developped a server in NodeJs",
-                "Single-handedly pentested the application (grey box approach)",
-                "Single-handedly deployed containers (backend, databaseand admin panel)",
-                "Setup backups for the database",
-                "Hiring other freelancers",
+                "Single-handedly developped a server in NodeJs.",
+                "Single-handedly pentested the application (grey box approach).",
+                "Single-handedly deployed containers (backend, databaseand admin panel).",
+                "Setup backups for the database.",
+                "Hiring other freelancers.",
               ]}
             />
             <Resumecontent
@@ -89,8 +89,8 @@ function Resume() {
               place="Paris, France"
               date="From June 2020 to July 2020"
               content={[
-                "Teaching students how to create a web site using PHP, HTML and CSS",
-                "Teaching students cybersecurity basics",
+                "Teaching students how to create a web site using PHP, HTML and CSS.",
+                "Teaching students cybersecurity basics.",
               ]}
             />
             <Resumecontent
@@ -100,8 +100,8 @@ function Resume() {
               place="Paris, France"
               date="From October 2019 to May 2020"
               content={[
-                "Single-handedly developed a web site using WordPress",
-                "Single-handedly developed and deployed an application in ASP.NET MVC aiming to help the company's employees tomanage orders",
+                "Single-handedly developed a web site using WordPress.",
+                "Single-handedly developed and deployed an application in ASP.NET MVC aiming to help the company's employees tomanage orders.",
               ]}
             />
             <Resumecontent
@@ -111,16 +111,16 @@ function Resume() {
               underliner="17.14%"
               date="From October 2019 to May 2020"
               content={[
-                "Development of an application in ASP.NET aiming to gather data about ATMs in real-time",
-                "Technical troubleshooting assistance",
+                "Development of an application in ASP.NET aiming to gather data about ATMs in real-time.",
+                "Technical troubleshooting assistance.",
               ]}
             />
             <h3 className="resume-title">IT Skills</h3>
             <Resumecontent
               title="NodeJS"
               content={[
-                "Implemented multiple APIs using express during my freelancing experience",
-                "Gave tens of hours of courses during my experience with Novolinko",
+                "Implemented multiple APIs using express during my freelancing experience.",
+                "Gave tens of hours of courses during my experience with Novolinko.",
               ]}
             />
             <Resumecontent
@@ -135,50 +135,50 @@ function Resume() {
               title="Python"
               content={[
                 "First programing language I have learnt.",
-                "Used during CTF challenges",
-                "Programing of APIs using flask",
+                "Used during CTF challenges.",
+                "Programing of APIs using flask.",
               ]}
             />
             <Resumecontent
               title="Docker"
               content={[
-                "Have deployed containers during my work with WTTP",
+                "Have deployed containers during my work with WTTP.",
               ]}
             />
             <Resumecontent
               title="ASP.NET"
               content={[
-                "Confortable around main tools (msf, nmap, burp., maltego..)",
+                "Confortable around main tools (msf, nmap, burp., maltego..).",
               ]}
             />
             <Resumecontent
               title="React-Native"
               content={[
-                "Implemented views on a mobile client during my free lancing experience with WTTP",
+                "Implemented views on a mobile client during my free lancing experience with WTTP.",
               ]}
             />
             <Resumecontent
               title="React"
               content={[
-                "Implemented this website",
+                "Implemented this website.",
               ]}
             />
             <Resumecontent
               title="PHP"
               content={[
-                "Used during my work experience with ISEG and Novolinko",
+                "Used during my work experience with ISEG and Novolinko.",
               ]}
             />
             <Resumecontent
               title="VBA"
               content={[
-                "Development of excel macros aiming to automate vulnerability analysis during my work at Aviva",
+                "Development of excel macros aiming to automate vulnerability analysis during my work at Aviva.",
               ]}
             />
             <Resumecontent
               title="ASM x86"
               content={[
-                "Used during CTFs and school project. I wish someday to work with this language",
+                "Used during CTFs and school project. I wish someday to work with this language.",
               ]}
             />
           </Col>
@@ -249,9 +249,9 @@ function Resume() {
               company='Trilingual'
               underliner="4%"
               content={[
-                "Was thaught arabic classes through my whole scholarship before high-school graduation",
+                "Was thaught arabic classes through my whole scholarship before high-school graduation.",
                 "Was born in morocco.",
-                <span style={{ marginLeft: '0%', color: "#1ff10c" }}>Also speaks moroccan dialect</span>,
+                <span style={{ marginLeft: '0%', color: "#1ff10c" }}>Also speaks moroccan dialect.</span>,
               ]}
             />
 
@@ -260,8 +260,8 @@ function Resume() {
               title="Parkour"
               date="Since 2019"
               content={[
-                "Started a parkour association at Epitech",
-                "Can do a backflip",
+                "Started a parkour association at Epitech.",
+                "Can do a backflip.",
               ]}
             />
             <Resumecontent
@@ -274,23 +274,23 @@ function Resume() {
               title="Running"
               date="Since 2020"
               content={[
-                "Participated in running sessions on Meetup"
+                "Participated in running sessions on Meetup."
               ]}
             />
             <Resumecontent
               title="Lock picking"
               date="Since 2020"
               content={[
-                "Started during lock-down, still a beginner",
-                "Fan of the lock-picking lawyer",
+                "Started during lock-down, still a beginner.",
+                "Fan of the lock-picking lawyer.",
               ]}
             />
             <Resumecontent
               title="OSINT"
               date="Since 2018"
               content={[
-                "Confortable around tools",
-                "Part of AlienVault",
+                "Confortable around tools.",
+                "Part of AlienVault.",
               ]}
             />
             <Resumecontent
@@ -298,38 +298,38 @@ function Resume() {
               date="Since 2016"
               content={[
                 <div>
-                  Participated to multiple cybersecurity events
+                  Participated to multiple cybersecurity events:
                   <ul>
                     <li>
-                      FIC 2017
+                      FIC 2017:
                     </li>
                     <ul>
                       <li>
-                        Participated to a project with the security association of Epitech Lille involving to catch and decrypt SMS recieved nearby
+                        Participated to a project with the security association of Epitech Lille involving to catch and decrypt SMS recieved nearby.
                       </li>
                     </ul>
                     <li>
-                      Le Hack 2019
+                      Le Hack 2019:
                     </li>
                     <ul>
                       <li>
-                        Participated to the CTF organised during the night (Was notamong the first ones)
+                        Participated to the CTF organised during the night (Was notamong the first ones).
                       </li>
                     </ul>
                     <li>
-                      Active on multiple CTFs plateformes
+                      Active on multiple CTFs plateformes:
                     </li>
                     <ul>
                       <li>
-                        HackTheBox
+                        HackTheBox:
                       </li>
                       <ul><li>Username: sheikh-spear</li></ul>
                       <li>
-                        TryHackMe
+                        TryHackMe:
                       </li>
                       <ul><li>Username: sheikh-spear</li></ul>
                       <li>
-                        Root-Me
+                        Root-Me:
                       </li>
                       <ul><li>Username: ismail4</li></ul>
                     </ul>
@@ -349,15 +349,15 @@ function Resume() {
               title="Project Management"
               
               content={[
-                "Managed a project on JIRA during my workexperience with Aviva",
-                "Concieved the implementation and deployment ofan API while worling with WTTP",
-                "Participated in the management of tens ofpentests while working with Aviva (I had the chance to work alone on some of them)"
+                "Managed a project on JIRA during my workexperience with Aviva.",
+                "Concieved the implementation and deployment ofan API while worling with WTTP.",
+                "Participated in the management of tens ofpentests while working with Aviva (I had the chance to work alone on some of them)."
               ]}
             />
             <Resumecontent
-              title="Microsoft Excel"  
+              title="Microsoft Office"
               content={[
-                "I Excel at it",
+                "I Excel at it.",
               ]}
             />
           </Col>
@@ -365,7 +365,7 @@ function Resume() {
         </Row>
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
+            <AiOutlineDownload style={{color: 'green'}}/>
             &nbsp;Download CV
           </Button>
         </Row>
